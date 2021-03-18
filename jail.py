91114a922286e11d6c10da9fcfc060b82190a8ce
Juan <juanalvarez9591@gmail.com>
2021-03-17 23:22:12 -0300

    Added game over condition if timer == timerlimit

--- jail.py
+++ jail.py
@@ -9,7 +9,11 @@ WIDTH = 600
 
 screen = pygame.display.set_mode([HEIGHT, WIDTH])
 
+# Timer things outside loop
 startTime = time.time()
+timer = "0" # logic license, so we can check if timer is != than x (x will be != 0 so it will fakely pass the first time) before defining x
+TIMERLIMIT = "30" # need to be a str
+
 
 def displayText(text, x, y, fonttype="Consolas", fontsize=30, fontcolor=(0,0,0)):
     pygame.font.init()
@@ -17,14 +21,24 @@ def displayText(text, x, y, fonttype="Consolas", fontsize=30, fontcolor=(0,0,0))
     textsurface = myfont.render(text, False, fontcolor)
     screen.blit(textsurface,(x,y))
 
+def GameOver():
+    if timer == TIMERLIMIT:
+        displayText("GAME OVER!", 10, 10)
+    else: pass
 
 # Game loop
 running = True
 while running:
     screen.fill((255, 255, 255))
 
-    timex = str(int(time.time() - startTime))
-    displayText(timex, HEIGHT/2, WIDTH/2)
+    # Timer
+    if timer != TIMERLIMIT:
+        timer = str(int(time.time() - startTime))
+        Timer = displayText(timer, HEIGHT/2, WIDTH/2)
+    else: 
+        GameOver()
+
+
 
     # Event handling
     for event in pygame.event.get():
